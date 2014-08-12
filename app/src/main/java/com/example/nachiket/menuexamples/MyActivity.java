d8dca1b1b99b2913c9232bf6d5919722ef83d311
Nachiket <nachiket@vertisinfotech.com>
2014-08-12 17:53:02 +0530

    Error fixes with a little new code

--- app/src/main/java/com/example/nachiket/menuexamples/MyActivity.java
+++ app/src/main/java/com/example/nachiket/menuexamples/MyActivity.java
@@ -1,44 +1,58 @@
 package com.example.nachiket.menuexamples;
-
-import android.app.DialogFragment;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.R;
+        	import android.app.DialogFragment;
+        	import android.os.Bundle;
+        	import android.support.v7.app.ActionBarActivity;
+        	import android.view.Menu;
+        	import android.view.MenuItem;
 
 
-public class MyActivity extends ActionBarActivity {
+        	public class MyActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_my);
-    }
+            	    @Override
+    	    protected void onCreate(Bundle savedInstanceState) {
+        	        super.onCreate(savedInstanceState);
+        	        setContentView(R.layout.activity_my);
+           }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
+            	    // This method creates the menu on the app
+                @Override
+    	    public boolean onCreateOptionsMenu(Menu menu) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            DialogFragment myDialogFragment = new MyDialogFragment();
-//
-//            myDialogFragment.show(getFragmentManager(),"theDialog");
-//            return true;
-//        }else if (id == R.id.exit_the_app){
-//            finish();
-//            return true;
-//        }
-        return super.onOptionsItemSelected(item);
-    }
-}
+        	        // Inflate the menu; this adds items to the action bar if it is present.
+        	        getMenuInflater().inflate(R.menu.my, menu);
+                return true;
+            }
+
+            	    // Called when a options menu item is selected
+                @Override
+    	    public boolean onOptionsItemSelected(MenuItem item) {
+        	        // Handle action bar item clicks here. The action bar will
+        	        // automatically handle clicks on the Home/Up button, so long
+       	        // as you specify a parent activity in AndroidManifest.xml.
+       	        int id = item.getItemId();
+
+        	        // We check what menu item was clicked and show a Toast
+        	        if (id == R.id.action_settings) {
+
+                        // A DialogFragment is a Fragment you can place over top
+            	            // the current Activity. A Fragment is like an interface
+            	            // block that you can place into an Activity.
+                        // The FrgamentManager allows you to interact with the
+                        // Fragment
+                        DialogFragment myFragment = new MyDialogFragment();
+
+                      myFragment.show(getFragmentManager(), "theDialog");
+
+                       return true;
+
+                   // If exit was clicked close the app
+                  } else if (id == R.id.exit_the_app) {
+                      finish();
+                       return true;
+                   }
+             return super.onOptionsItemSelected(item);
+         }
+
+
+           	}
